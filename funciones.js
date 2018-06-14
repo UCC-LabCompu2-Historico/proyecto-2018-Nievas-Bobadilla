f33738c7b4d9bc5eb4c738a60ebf05dbe2abe8e7
FrancoBobadilla <1709673@ucc.edu.ar}>
2018-06-14 10:00:43 -0300

    se arreglo problema de estiramiento del canvas

--- funciones.js
+++ funciones.js
@@ -13,7 +13,7 @@ function Cara() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     ctx.beginPath();
-    ctx.arc(150, 150, 50, 0, Math.PI*2, true);
+    ctx.arc(50, 50, 10, 0, Math.PI*2, true);
     ctx.stroke();
     ctx.closePath();
 }
