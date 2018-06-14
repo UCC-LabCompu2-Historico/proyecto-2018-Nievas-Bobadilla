526024a70bae621b8bae5d44111754c071faacb4
FrancoBobadilla <1709673@ucc.edu.ar}>
2018-06-14 08:47:57 -0300

    se cambio tamanio de canvas a 300

--- Canvas.js
+++ Canvas.js
@@ -1,16 +1,7 @@
-function Cara(){
-    // Toma el elemento canvas usando el DOM
-    var canvas = document.getElementById('mycanvas');
-
-        var ctx = canvas.getContext('2d');
-
-        // Dibuja Lineas
-        for (i=0;i<10;i++){
-            ctx.lineWidth = 1+i;
-            ctx.beginPath();
-            ctx.moveTo(5+i*14,5);
-            ctx.lineTo(5+i*14,140);
-            ctx.stroke();
-        }
-
+function Cara() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(150, 150, 50, 0, Math.PI*2, true);
+    ctx.stroke();
 }
