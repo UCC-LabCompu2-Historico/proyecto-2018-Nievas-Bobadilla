3c51667ac441c55ac7e9d4de058e9ccd2420939c
FrancoBobadilla <1709673@ucc.edu.ar}>
2018-06-14 09:43:13 -0300

    se implemento funciones para transmitir datos de una pagina a otra

--- funciones.js
+++ funciones.js
@@ -1,4 +1,13 @@
-cargar
+function cargarValor(){
+    var planeta = document.getElementById("selectPlaneta").value;
+    var urlVar = 'simulador.html#' + planeta;
+    window.location.assign(urlVar);
+}
+
+function descargarValor(){
+    var planeta = window.location.hash.split('#')[1];
+    console.log(planeta);
+}
 function Cara() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
